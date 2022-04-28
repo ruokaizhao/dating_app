b6c65d24a89ecd3b18db6412a89b38df7cf0f11c
Ruokai <lelouch_l@icloud.com>
2022-04-27 23:43:01 -0400

    React setup

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Page Count: {count}</h1>
+    </div>
+  );
+}
+
+export default App;
