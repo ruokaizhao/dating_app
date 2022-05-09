dcb8b54e8f3dfb14bc3a515911c1780a43bce0f8
Ruokai <lelouch_l@icloud.com>
2022-05-09 00:38:59 -0400

    Build Home and NavBar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react'
+import Auth from './Auth'
 import NavBar from './NavBar'
 
 function Home() {
   const [user, setUser] = useState({})
+  const [showAuth, setShowAuth] = useState(false)
 
   function handleClick() {
-    console.log('clicked')
+    setShowAuth(true)
   }
-
 
   return (
     <div className="background">
-      <NavBar user={user} />
+      <NavBar user={user} showAuth={showAuth} setShowAuth={setShowAuth} />
       <div className="home">      
         <h1>Swipe Right®</h1>
-        <button className="primary-button" onClick={handleClick}>
+        <button className="primary-button" onClick={() => setShowAuth(true)}>
           {user.id ? 'Logout' : "Create account"}
         </button>
+        {showAuth && <Auth setShowAuth={setShowAuth} />}
       </div>
     </div>    
   )
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,13 +2,17 @@ import React from 'react'
 import colorLogo from '../images/color_logo.png'
 import whiteLogo from '../images/white_logo.png'
 
-function NavBar({ user, minimal }) {
+function NavBar({ user, minimal, showAuth, setShowAuth }) {
   return (
     <nav>
       <div className="logo-container">
         <img className="logo" src={minimal ? colorLogo : whiteLogo} />
       </div>
-      {!user.id && <button className="nav-button">Login</button>}
+      {!user.id
+       && 
+      <button className="nav-button" disabled={showAuth} onClick={() => setShowAuth(true)}>
+        Login
+      </button>}
     </nav>
   )
 }
