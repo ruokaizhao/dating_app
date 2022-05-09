25df33bcc57ffd018d1378b13462e3e936204d06
Ruokai <lelouch_l@icloud.com>
2022-05-09 00:39:30 -0400

    Build Auth component with Formik

---
+++ client/src/components/Auth.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
+function Auth({ setShowAuth }) {
+  const isSignUp = true
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      password2: ''
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid email address').required('Required'),
+      password: Yup.string().min(6, 'Must be at least 6 characters'),
+      password2: Yup.string().when('password', {
+        is: val => (val && val.length > 0 ? true : false),
+        then: Yup.string().oneOf(
+          [Yup.ref('password')],
+          'Both password need to be the same'
+        )
+      })
+    }),
+    onsubmit: values => handleSubmit(values)
+  })
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
+
+  
+
+  return (
+    <div className="auth">
+      <div className="close-icon" onClick={() => setShowAuth(false)}>ⓧ</div>
+      <h1>{isSignUp ? "CREATE ACCOUNT" : "LOG IN"}</h1>
+      <p>By clicking Log In, you agree to our Terms. Learn how we process your data in our Privacy Policy and Cookie Policy.</p>
+      <form onsubmit={formik.handleSubmit}>
+        <input type='email' name='email' id='email' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} />
+        {formik.touched.email && formik.errors.email && <div>{formik.errors.email}</div>}
+        <input type='password' name='password' id='password' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.password} />
+        {formik.touched.password && formik.errors.password && <div>{formik.errors.password}</div>}
+        <input type='password' name='password2' id='password2' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.password2} />
+        {formik.touched.password2 && formik.errors.password2 && <div>{formik.errors.password2}</div>}
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Auth
