85fe67a18be5208adf4500d42751dc53c8e0e94f
Ruokai <lelouch_l@icloud.com>
2022-05-10 05:32:12 -0400

    Build Dashboard using react-tinder-card and add stylings

---
+++ client/src/components/ChatContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ChatContainer() {
+  return (
+    <div className="chat-container">
+      
+
+    </div>
+  )
+}
+
+export default ChatContainer
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TinderCard from 'react-tinder-card'
+import ChatContainer from './ChatContainer'
+
+const characters = [
+  {
+    name: 'Richard Hendricks',
+    url: 'https://i.imgur.com/oPj4A8u.jpeg'
+  },
+  {
+    name: 'Erlich Bachman',
+    url: 'https://i.imgur.com/oPj4A8u.jpeg'
+  },
+  {
+    name: 'Monica Hall',
+    url: 'https://i.imgur.com/oPj4A8u.jpeg'
+  },
+  {
+    name: 'Jared Dunn',
+    url: 'https://i.imgur.com/oPj4A8u.jpeg'
+  },
+  {
+    name: 'Dinesh Chugtai',
+    url: 'https://i.imgur.com/oPj4A8u.jpeg'
+  }
+]
 
 function Dashboard() {
+  const [lastDirection, setLastDirection] = useState()
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+  }
+
   return (
-    <div>Dashboard</div>
+    <div className="dashboard">
+      <ChatContainer />
+      <div className="swipe-container">
+        <div className="card-container">
+          {characters.map((character) =>
+            <TinderCard className="swipe" key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+              <div style={{ backgroundImage: "url(" + character.url + ")" }} className="card">
+                <h3>{character.name}</h3>
+              </div>
+            </TinderCard>
+          )}
+        </div>
+        {lastDirection ? <h2 className="swipe-info">You swiped {lastDirection}</h2> : <h2 className='infoText' />}
+      </div>
+    </div>
   )
 }
 
