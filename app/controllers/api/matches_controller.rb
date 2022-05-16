9bb8537a2809d589d71184d2de6262be5c22ba5d
Ruokai <lelouch_l@icloud.com>
2022-05-15 23:56:45 -0400

    Create Match.new_match method on backend to handle incoming matches

--- app/controllers/api/matches_controller.rb
+++ app/controllers/api/matches_controller.rb
@@ -1,14 +1,14 @@
 class Api::MatchesController < ApplicationController
 
   def create
-    match = Match.create!(match_params)
+    match = Match.new_match(params)
     render json: match, status: :created
   end
 
-  private
+  # private
 
-  def match_params
-    params.permit(:user_id, :browsed_user_id, :liked)
-  end
+  # def match_params
+  #   params.permit(:user_id, :browsed_user_id, :liked)
+  # end
 
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,24 @@
+require 'securerandom'
+
 class Match < ApplicationRecord
   belongs_to :user
   belongs_to :browsed_user, class_name: "User", foreign_key: "browsed_user_id"
+
+  def self.new_match(params)
+    browsed_user = User.find(params[:browsed_user_id])
+    exist_match = browsed_user.matches.where(browsed_user_id: params[:user_id])
+    if exist_match[0]
+      uuid = exist_match[0][:pair_id]
+      match = Match.create!(pair_id: uuid, user_id: params[:user_id], browsed_user_id: params[:browsed_user_id], liked: params[:liked])
+      if exist_match[0][:liked] == true && params[:liked] == true
+        UserMatchChannel.broadcast_to(browsed_user, "Someone liked you.")
+      end
+      return match
+    else
+      uuid = SecureRandom.uuid
+      match = Match.create!(pair_id: uuid, user_id: params[:user_id], browsed_user_id: params[:browsed_user_id], liked: params[:liked])
+      return match
+    end
+  end
+
 end
