e9ceec2cf6e4c9e6db7a3ca0e73fee42f338db26
Ruokai <lelouch_l@icloud.com>
2022-05-20 23:20:05 -0400

    Build menu button for mobile view

--- client/src/components/Chat/ChatHeader.js
+++ client/src/components/Chat/ChatHeader.js
@@ -3,8 +3,16 @@ import React from 'react'
 function ChatHeader({ user }) {
   return (
     <div className="chat-header">
-      <img className="profile-photo" src={user.url1} alt="profile-photo" />
-      <h1>{user.first_name}</h1>
+      <div className="chat-header-photo-name">
+        <img className="profile-photo" src={user.url1} alt="profile" />
+        <h1>{user.first_name}</h1>    
+      </div>
+      <div className="menu-icon">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
     </div>
   )
 }
