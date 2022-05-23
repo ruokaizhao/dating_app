e051fe4cae8cd3840328b7982bf1ee75879f97db
Ruokai <lelouch_l@icloud.com>
2022-05-23 17:45:44 -0400

    Prepare for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
+  config.action_cable.url = "wss://dating-app-react-ruby-on-rails.herokuapp.com/"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
