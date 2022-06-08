017a5e1abd72a95f37b74660bbe18c5a2366988f
Ruokai <lelouch_l@icloud.com>
2022-06-08 02:34:41 -0400

    Prepare for deployment to Heroku

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 import actionCable from 'actioncable'
 
 const CableApp = {}
-CableApp.cable = actionCable.createConsumer('ws://localhost:3000/cable')
-// CableApp.cable = actionCable.createConsumer('wss://dating-app-react-ruby-on-rails.herokuapp.com/cable')
+// CableApp.cable = actionCable.createConsumer('ws://localhost:3000/cable')
+CableApp.cable = actionCable.createConsumer('wss://dating-app-react-ruby-on-rails.herokuapp.com/cable')
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
